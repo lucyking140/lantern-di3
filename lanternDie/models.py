0799ff5b1fa28637a77381b72aa6e8008b49a396
lucyking140 <lucyking140@gmail.com>
2023-09-07 19:38:04 -0400

    adding post_save to models.py

--- lanternDie/models.py
+++ lanternDie/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 class Profiles(models.Model):
     user = models.OneToOneField(User, default="01", on_delete=models.CASCADE, related_name="profile") #creating profiles
